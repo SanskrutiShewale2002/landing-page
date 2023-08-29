119ea8e0af9d122706baa12a0e33fdbfbf6851a4
sanskruti shewale <sanskrutishewale4@gmail.com>
2023-08-29 14:29:38 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+// Smooth scrolling for navigation links
+document.addEventListener("DOMContentLoaded", function() {
+    const navLinks = document.querySelectorAll(".nav-links a");
+
+    navLinks.forEach(link => {
+        link.addEventListener("click", function(event) {
+            event.preventDefault();
+            const targetId = this.getAttribute("href");
+            const targetSection = document.querySelector(targetId);
+            if (targetSection) {
+                window.scrollTo({
+                    top: targetSection.offsetTop,
+                    behavior: "smooth"
+                });
+            }
+        });
+    });
+});
